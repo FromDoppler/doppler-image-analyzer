e0e78d33662e1e6b8ddadf558f8f412ba128cbea
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-02-07 11:48:55 -0300

    Add response classes

---
+++ Doppler.ImageAnalysisApi/Api/ResponseErrorDetails.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doppler.ImageAnalysisApi.Api
+{
+    public class ResponseErrorDetails : ValidationProblemDetails
+    {
+        public ResponseErrorDetails()
+        {
+        }
+
+        public ResponseErrorDetails(ModelStateDictionary modelState)
+            : base(modelState)
+        {
+        }
+
+        public ResponseErrorDetails(IDictionary<string, string[]> errors)
+            : base(errors)
+        {
+        }
+
+        /// <summary>
+        /// The intention of this property is to easily show error exception details in a separate field not consumed by the UI
+        /// Developers could check the browser console logs to see the errors details.
+        /// </summary>
+        public string ExceptionMessage { get; set; }
+    }
+}
