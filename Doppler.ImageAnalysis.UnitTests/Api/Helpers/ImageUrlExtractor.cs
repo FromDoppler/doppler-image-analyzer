9ef9733e04c57c199f8c489579a75291855b9a2f
Alejandro Rovella <alrovella@gmail.com>
2023-02-07 12:24:38 -0300

    Fix first test

--- Doppler.ImageAnalysis.UnitTests/Api/Helpers/ImageUrlExtractor.cs
+++ Doppler.ImageAnalysis.UnitTests/Api/Helpers/ImageUrlExtractor.cs
@@ -9,12 +9,12 @@ namespace Doppler.ImageAnalysis.UnitTests.Api.Helpers
         [Fact]
         public void ExtractImageUrls_GivenValidHtml_ShouldReturnListOfImageUrls()
         {
-            string html = "<html><body><img src='image1.jpg' /><img src='image2.jpg' /></body></html>";
+            string html = "<html><body><img src='http://www.test.com/image1.jpg' /><img src='http://test.com/image2.jpg' /></body></html>";
             var extractor = new ImageAnalysisApi.Helpers.ImageUrlExtractor();
 
             List<string> imageUrls = extractor.Extract(html);
 
-             Assert.Equal(new List<string> { "image1.jpg", "image2.jpg" }, imageUrls);
+             Assert.Equal(new List<string> { "http://www.test.com/image1.jpg", "http://test.com/image2.jpg" }, imageUrls);
         }
 
     }
