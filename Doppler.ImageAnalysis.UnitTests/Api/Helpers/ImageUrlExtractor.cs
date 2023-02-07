accf11ab4509b342c9547bc1a7b85a101a3b6f03
Alejandro Rovella <alrovella@gmail.com>
2023-02-07 11:45:42 -0300

    Init UT

---
+++ Doppler.ImageAnalysis.UnitTests/Api/Helpers/ImageUrlExtractor.cs
@@ -0,0 +1,21 @@
+﻿using Doppler.ImageAnalysisApi.Helpers;
+using Xunit;
+
+namespace Doppler.ImageAnalysis.UnitTests.Api.Helpers
+{
+    public class ImageUrlExtractor
+    {
+
+        [Fact]
+        public void ExtractImageUrls_GivenValidHtml_ShouldReturnListOfImageUrls()
+        {
+            string html = "<html><body><img src='image1.jpg' /><img src='image2.jpg' /></body></html>";
+            var extractor = new ImageAnalysisApi.Helpers.ImageUrlExtractor();
+
+            List<string> imageUrls = extractor.Extract(html);
+
+             Assert.Equal(new List<string> { "image1.jpg", "image2.jpg" }, imageUrls);
+        }
+
+    }
+}
