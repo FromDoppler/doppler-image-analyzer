9a71a61f0383ce41f0376db8f35880af4e1bf74f
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-02-10 18:33:25 -0300

    Add controller, endpoint and handler

---
+++ Doppler.ImageAnalysisApi/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Doppler.ImageAnalysisApi.Api;
+using System.Net;
+
+namespace Doppler.ImageAnalysisApi.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        public const string UnexpectedErrorKey = "UNEXPECTED_ERROR";
+
+        public static Response<T> ToResponse<T>(this Exception exception, string referenceId = null)
+        {
+            var description = referenceId != null
+                ? $"ReferenceId: {referenceId}"
+                : exception.ToString(); // Kept for backward compatibility.
+            return Response.GetResponseError<T>(statusCode: HttpStatusCode.InternalServerError, errorKey: UnexpectedErrorKey, errorDescription: description, exception: exception);
+        }
+
+        public static Response ToResponse(this Exception exception, string referenceId = null)
+        {
+            var description = referenceId != null
+                ? $"ReferenceId: {referenceId}"
+                : exception.ToString(); // Kept for backward compatibility.
+            return Response.GetResponseError(statusCode: HttpStatusCode.InternalServerError, errorKey: UnexpectedErrorKey, errorDescription: description, exception: exception);
+        }
+    }
+}
