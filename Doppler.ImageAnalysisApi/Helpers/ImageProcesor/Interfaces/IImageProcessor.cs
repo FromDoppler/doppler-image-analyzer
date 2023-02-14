740f4b9eedd54a1724698997c13e4c67433fbec5
Hugo Velazquez <hvelazquez@makingsense.com>
2023-02-14 13:11:22 -0600

    Image-Processor, Change 1, Adding structure and service to process Images

---
+++ Doppler.ImageAnalysisApi/Helpers/ImageProcesor/Interfaces/IImageProcessor.cs
@@ -0,0 +1,6 @@
+﻿namespace Doppler.ImageAnalysisApi.Helpers.ImageProcesor.Interfaces;
+
+public interface IImageProcessor
+{
+    Task<IEnumerable<IImageConfidence>?> ProcessImage(string url, CancellationToken cancellationToken = default);
+}
