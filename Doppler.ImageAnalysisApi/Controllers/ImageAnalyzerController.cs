0ab1a4eb76f37e9de2aefcc74b26b345449bca12
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-02-16 16:38:20 -0300

    Adjust swagger schema

--- Doppler.ImageAnalysisApi/Controllers/ImageAnalyzerController.cs
+++ Doppler.ImageAnalysisApi/Controllers/ImageAnalyzerController.cs
@@ -1,5 +1,6 @@
 ﻿using Doppler.ImageAnalysisApi.Api;
 using Doppler.ImageAnalysisApi.Features.Analysis;
+using Doppler.ImageAnalysisApi.Features.Analysis.Requests;
 using Doppler.ImageAnalysisApi.Features.Analysis.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -16,18 +17,18 @@ namespace Doppler.ImageAnalysisApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeHtml(string htmlToAnalyze, CancellationToken cancellationToken)
+        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeHtml(AnalyzeHtmlRequest request, CancellationToken cancellationToken)
         {
-            var command = new AnalyzeHtml.Command { HtmlToAnalize = htmlToAnalyze };
+            var command = new AnalyzeHtml.Command { HtmlToAnalize = request.HtmlToAnalize, AllLabels = request.AllLabels };
             var response = await _mediator.Send(command, cancellationToken);
 
             return HandleResponse(response, "Returned image analysis");
         }
 
         [HttpPost]
-        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeImageList(List<string> imageUrls, CancellationToken cancellationToken)
+        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeImageList(AnalyzeImageListRequest request, CancellationToken cancellationToken)
         {
-            var command = new AnalyzeImageList.Command { ImageUrls = imageUrls };
+            var command = new AnalyzeImageList.Command { ImageUrls = request.ImageUrls, AllLabels = request.AllLabels };
             var response = await _mediator.Send(command, cancellationToken);
 
             return HandleResponse(response, "Returned image analysis");
