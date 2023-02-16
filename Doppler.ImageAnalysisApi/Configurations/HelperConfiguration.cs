21013ef7e2de2653880c45ab297ddecec10e7a0a
jpetrozzi-makingsense <jpetrozzi@makingsense.com>
2023-02-16 15:10:33 -0300

    Add endpoint to analyze list of image urls

--- Doppler.ImageAnalysisApi/Configurations/HelperConfiguration.cs
+++ Doppler.ImageAnalysisApi/Configurations/HelperConfiguration.cs
@@ -2,6 +2,7 @@
 using Doppler.ImageAnalysisApi.Helpers;
 using Doppler.ImageAnalysisApi.Helpers.AmazonRekognition;
 using Doppler.ImageAnalysisApi.Helpers.AmazonS3;
+using Doppler.ImageAnalysisApi.Helpers.ImageAnalysis;
 using Doppler.ImageAnalysisApi.Helpers.ImageProcesor;
 using Doppler.ImageAnalysisApi.Helpers.ImageProcesor.Interfaces;
 using Doppler.ImageAnalysisApi.Helpers.Web;
@@ -16,6 +17,7 @@ public static class HelperConfiguration
         services.AddSingleton(_ => config);
         services.AddAmazonS3(config!.Amazon!);
         services.AddAmazonRekognition(config!.Amazon!);
+        services.AddScoped<IAnalysisOrchestrator, AnalysisOrchestrator>();
         services.AddScoped<IImageUrlExtractor, ImageUrlExtractor>();
         services.AddScoped<IImageProcessor, ImageProcessor>();
         services.AddHttpClient<IImageDownloadClient, ImageDownloadClient>(httpClient =>
--- Doppler.ImageAnalysisApi/Controllers/ImageAnalyzerController.cs
+++ Doppler.ImageAnalysisApi/Controllers/ImageAnalyzerController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Doppler.ImageAnalysisApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ImageAnalyzerController : TaggerControllerBase
     {
@@ -16,8 +16,18 @@ namespace Doppler.ImageAnalysisApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeHtml(AnalyzeHtml.Command command, CancellationToken cancellationToken)
+        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeHtml(string htmlToAnalyze, CancellationToken cancellationToken)
         {
+            var command = new AnalyzeHtml.Command { HtmlToAnalize = htmlToAnalyze };
+            var response = await _mediator.Send(command, cancellationToken);
+
+            return HandleResponse(response, "Returned image analysis");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Response<List<ImageAnalysisResponse>>>> AnalyzeImageList(List<string> imageUrls, CancellationToken cancellationToken)
+        {
+            var command = new AnalyzeImageList.Command { ImageUrls = imageUrls };
             var response = await _mediator.Send(command, cancellationToken);
 
             return HandleResponse(response, "Returned image analysis");
