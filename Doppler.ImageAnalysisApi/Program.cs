52ac5147dd08c648e04b91688c56f238c67195f0
Andres Moschini <andresmoschini@gmail.com>
2023-02-27 16:24:17 -0300

    chore: update MediatR boilerplate
    See https://github.com/jbogard/MediatR/wiki/Migration-Guide-11.x-to-12.0#service-registration-through-configuration-object-exclusively

--- Doppler.ImageAnalysisApi/Program.cs
+++ Doppler.ImageAnalysisApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 builder.Services.AddOperationsLogic(appConfig);
 
 var app = builder.Build();
