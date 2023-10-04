3e10c8adf7eb9b6771a35af16dd422988565856f
leoslopez <llopez@makingsense.com>
2023-10-04 18:48:20 -0300

    test: verify repository invoke when result is success

--- Doppler.ImageAnalyzer.UnitTests/Api/Controllers/ImageAnalyzerControllerTests.cs
+++ Doppler.ImageAnalyzer.UnitTests/Api/Controllers/ImageAnalyzerControllerTests.cs
@@ -53,6 +53,52 @@ public class ImageAnalyzerControllerTests
     }
 
     [Fact]
+    public async Task AnalyzeHtml_ShouldCallImageAnalysisResultRepositoryAndReturnResultId_WhenSuccess()
+    {
+        // Arrange
+        var html = "<html><div><img src='https://www.test.com/test.jpg'></div></html>";
+
+        var response = new Response<List<ImageAnalysisResponse>>()
+        {
+            Payload = new List<ImageAnalysisResponse>()
+            {
+                new ImageAnalysisResponse()
+                {
+                    ImageUrl = "https://www.test.com/test.jpg",
+                    AnalysisDetail = new List<ImageAnalysisDetailResponse>(),
+                }
+            }
+        };
+
+        var saveAsyncResultId = "aResultId";
+
+        _mediatorMock.Setup(m => m.Send(It.IsAny<IRequest<Response<List<ImageAnalysisResponse>>>>(), default))
+                     .ReturnsAsync(response);
+
+        _imageAnalysisResultRepositoryMock.Setup(m => m.SaveAsync(It.IsAny<List<ImageAnalysisResponse>>()))
+                     .ReturnsAsync(saveAsyncResultId);
+
+        var controller = new ImageAnalyzerController(_mediatorMock.Object, _loggerMock.Object, _imageAnalysisResultRepositoryMock.Object);
+        var request = new AnalyzeHtmlRequest { HtmlToAnalize = html, AnalysisType = "ModerationContent" };
+
+        // Act
+        var result = await controller.AnalyzeHtml(request, default);
+
+        // Assert
+        Assert.NotNull(result);
+        _imageAnalysisResultRepositoryMock.Verify(x => x.SaveAsync(It.IsAny<List<ImageAnalysisResponse>>()), Times.Once());
+
+        // Obtain the result of the call
+        var callResult = Assert.IsType<Microsoft.AspNetCore.Mvc.OkObjectResult>(result.Result);
+
+        // Obtain "Value" and convert to "AnalysisResultResponse"
+        var analysisResultResponse = Assert.IsType<AnalysisResultResponse>(callResult.Value);
+
+        // Assert the result contains the "AnalysisResultId" returned by the Repository
+        Assert.True(analysisResultResponse.AnalysisResultId == saveAsyncResultId);
+    }
+
+    [Fact]
     public async Task AnalyzeImageList_ShouldCallMediator_WhenSuccess()
     {
         var imageUrls = new List<string>{
